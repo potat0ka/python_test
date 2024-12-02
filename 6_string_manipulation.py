6b0fd7f2faa7b2eafa51682c7fbbb4037aaffe9d
Bigendra Shrestha <bige.stha88@gmail.com>
2024-12-02 17:42:36 +0545

    3rd day

---
+++ 6_string_manipulation.py
@@ -0,0 +1,12 @@
+first_name = "aris"
+second_name = "sarad"
+
+print (first_name+second_name) #concat
+
+a = "aris"
+b = 9
+print (a+str(b))
+
+first_sentence = " hello I am from mars  "
+
+print(first_sentence.strip())
